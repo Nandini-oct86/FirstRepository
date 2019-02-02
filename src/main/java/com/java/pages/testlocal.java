7ed2e40e6cc5aa6c6c5548853d15aae999d03b5a
Nandini-oct86 <47260862+Nandini-oct86@users.noreply.github.com>
2019-02-02 08:27:41 -0500

    Update testlocal.java
    added int commands

--- src/main/java/com/java/pages/testlocal.java
+++ src/main/java/com/java/pages/testlocal.java
@@ -5,6 +5,13 @@ public class testlocal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
  System.out.println("jkhkh");
+		int a = 10;
+		int b= 20;
+		int c = 30;
+		int d= 40;
+		int e =50;
+		int f=78;
+		
 	}
 
 }
